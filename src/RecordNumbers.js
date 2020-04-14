d688df22aabca98eec5ec6b39b260188452f3802
spainisnotequal <spainisnotequal@gmail.com>
2020-04-14 22:59:09 +0200

    Add code copied from GitHub ("react-media-recorder")

--- src/RecordNumbers.js
+++ src/RecordNumbers.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const RecordNumbers = () => {
   // Hooks for the Timer
@@ -6,6 +6,28 @@ const RecordNumbers = () => {
   const [isActive, setIsActive] = useState(false);
 
   const array = ["zero", "one", "two", "three", "four", "five"];
+
+  const mediaStream = useRef(null);
+  const mediaRecorder = useRef(null);
+  const mediaChuncks = useRef([]);
+  const [status, setStatus] = useState("idle");
+  const [mediaBlobUrl, setMediaBlobUrl] = useState(null);
+  const [error, setError] = useState("NONE");
+
+  const getMediaStream = async () => {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        mediaStream.current = stream;
+      })
+      .catch((err) => {
+        console.log("The following getUserMedia error occured: " + err);
+      });
+  };
+
+  useEffect(() => {
+    getMediaStream();
+  }, []);
 
   useEffect(() => {
     let interval = null;
@@ -20,6 +42,60 @@ const RecordNumbers = () => {
       clearInterval(interval);
     };
   }, [isActive, index, array]);
+
+  const startRecording = async () => {
+    setError("NONE");
+    if (!mediaStream.current) {
+      await getMediaStream();
+    }
+    if (mediaStream.current) {
+      mediaRecorder.current = new MediaRecorder(mediaStream.current);
+      mediaRecorder.current.ondataavailable = onRecordingActive;
+      mediaRecorder.current.onstop = onRecordingStop;
+      mediaRecorder.current.onerror = () => {
+        setError("NO_RECORDER");
+        console.log(error);
+        setStatus("idle");
+        console.log(status);
+      };
+      mediaRecorder.current.start();
+      setStatus("recording");
+      console.log(status);
+    }
+  };
+
+  const onRecordingActive = ({ data }) => {
+    mediaChuncks.current.push(data);
+  };
+
+  const onRecordingStop = () => {
+    const blobProperty = { type: "audio/wav" };
+    const blob = new Blob(mediaChuncks.current, blobProperty);
+    const url = URL.createObjectURL(blob);
+    setStatus("stopped");
+    console.log(status);
+    setMediaBlobUrl(url);
+    console.log(mediaBlobUrl);
+  };
+
+  const pauseRecording = () => {
+    if (mediaRecorder.current && mediaRecorder.current.state === "recording") {
+      mediaRecorder.current.pause();
+    }
+  };
+  const resumeRecording = () => {
+    if (mediaRecorder.current && mediaRecorder.current.state === "paused") {
+      mediaRecorder.current.resume();
+    }
+  };
+
+  const stopRecording = () => {
+    if (mediaRecorder.current) {
+      setStatus("stopping");
+      console.log(status);
+      mediaRecorder.current.stop();
+    }
+  };
 
   const toggle = () => {
     // Start the timer
@@ -51,6 +127,12 @@ const RecordNumbers = () => {
       <button onClick={stop} disabled={isActive ? false : true}>
         Stop
       </button>
+      <br />
+      <br />
+      <button onClick={startRecording}>Start recording</button>
+      <button onClick={pauseRecording}>Pause recording</button>
+      <button onClick={resumeRecording}>Resume recording</button>
+      <button onClick={stopRecording}>Stop recording</button>
     </>
   );
 };
