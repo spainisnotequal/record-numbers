cdc0acc093a68011f335f2345205064cacd6883f
spainisnotequal <spainisnotequal@gmail.com>
2020-04-14 23:50:57 +0200

    Add first solution to record audio

--- src/RecordNumbers.js
+++ src/RecordNumbers.js
@@ -7,28 +7,6 @@ const RecordNumbers = () => {
 
   const array = ["zero", "one", "two", "three", "four", "five"];
 
-  const mediaStream = useRef(null);
-  const mediaRecorder = useRef(null);
-  const mediaChuncks = useRef([]);
-  const [status, setStatus] = useState("idle");
-  const [mediaBlobUrl, setMediaBlobUrl] = useState(null);
-  const [error, setError] = useState("NONE");
-
-  const getMediaStream = async () => {
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then((stream) => {
-        mediaStream.current = stream;
-      })
-      .catch((err) => {
-        console.log("The following getUserMedia error occured: " + err);
-      });
-  };
-
-  useEffect(() => {
-    getMediaStream();
-  }, []);
-
   useEffect(() => {
     let interval = null;
     if (isActive && index < array.length - 1) {
@@ -43,39 +21,23 @@ const RecordNumbers = () => {
     };
   }, [isActive, index, array]);
 
-  const startRecording = async () => {
-    setError("NONE");
-    if (!mediaStream.current) {
-      await getMediaStream();
-    }
-    if (mediaStream.current) {
-      mediaRecorder.current = new MediaRecorder(mediaStream.current);
-      mediaRecorder.current.ondataavailable = onRecordingActive;
-      mediaRecorder.current.onstop = onRecordingStop;
-      mediaRecorder.current.onerror = () => {
-        setError("NO_RECORDER");
-        console.log(error);
-        setStatus("idle");
-        console.log(status);
-      };
-      mediaRecorder.current.start();
-      setStatus("recording");
-      console.log(status);
-    }
-  };
+  let mediaRecorder = useRef(null);
+  let mediaChuncks = useRef([]);
 
-  const onRecordingActive = ({ data }) => {
-    mediaChuncks.current.push(data);
-  };
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => (mediaRecorder.current = new MediaRecorder(stream)))
+      .catch((err) => {
+        console.log("The following getUserMedia error occured: " + err);
+      });
+  }, []);
 
-  const onRecordingStop = () => {
-    const blobProperty = { type: "audio/wav" };
-    const blob = new Blob(mediaChuncks.current, blobProperty);
-    const url = URL.createObjectURL(blob);
-    setStatus("stopped");
-    console.log(status);
-    setMediaBlobUrl(url);
-    console.log(mediaBlobUrl);
+  const startRecording = () => {
+    mediaRecorder.current.start();
+    console.log("recording");
+    mediaRecorder.current.ondataavailable = ({ data }) =>
+      mediaChuncks.current.push(data);
   };
 
   const pauseRecording = () => {
@@ -91,48 +53,44 @@ const RecordNumbers = () => {
 
   const stopRecording = () => {
     if (mediaRecorder.current) {
-      setStatus("stopping");
-      console.log(status);
+      console.log("stopping");
       mediaRecorder.current.stop();
+
+      const blobProperty = { type: "audio/wav" };
+      const blob = new Blob(mediaChuncks.current, blobProperty);
+      const url = URL.createObjectURL(blob);
+      console.log("stopped");
+      console.log(url);
     }
   };
 
   const toggle = () => {
     // Start the timer
     setIsActive(!isActive);
+    startRecording();
   };
 
   const reset = () => {
     // Stop the timer and go to the first element
     setIsActive(false);
     setIndex(0);
+    stopRecording();
   };
 
   const stop = () => {
     // Stop the timer and go to the last element
     setIsActive(false);
     setIndex(array.length - 1);
+    stopRecording();
   };
 
   return (
     <>
       <h2>{array[index]}</h2>
       <br />
-      <button onClick={toggle} disabled={isActive ? true : false}>
-        {isActive ? "Pause" : "Start"}
-      </button>
-      <button onClick={reset} disabled={isActive ? false : true}>
-        Reset
-      </button>
-      <button onClick={stop} disabled={isActive ? false : true}>
-        Stop
-      </button>
-      <br />
-      <br />
-      <button onClick={startRecording}>Start recording</button>
-      <button onClick={pauseRecording}>Pause recording</button>
-      <button onClick={resumeRecording}>Resume recording</button>
-      <button onClick={stopRecording}>Stop recording</button>
+      <button onClick={toggle}>{isActive ? "Pause" : "Start"}</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={stop}>Stop</button>
     </>
   );
 };
